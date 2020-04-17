bffc33e5ae46061edd24744903dbd58926aec03d
fboyle2001 <7895247+fboyle2001@users.noreply.github.com>
2020-04-17 19:24:49 +0100

    Govee packet structure and RGB

---
+++ server/bluetooth_controller.py
@@ -0,0 +1,41 @@
+#import pexpect
+#gatt = pexpect.spawn("gatttool -I")
+
+class GoveePacket:
+    def __init__(self, identifier):
+        self.identifier = identifier
+        self.data = 0
+        self.sig = identifier
+
+    def set_data_byte(self, pos, intv):
+        assert 0 <= intv <= 255
+        pos_val = intv << ((17 - pos) * 8)
+        clear_mask = (2 ** 8) ** 20 - 1
+        #need a way to wipe a byte, use an AND with 111..1000000001..111
+        self.data = self.data | pos_val
+
+    def set_data(self, data):
+        for index, byte in enumerate(data):
+            self.set_data_byte(index, byte)
+            self.sig = self.sig ^ byte
+
+    def generate_packet(self):
+        byte_no = 0
+        sgm_id = fix_hex_length(self.identifier, 1)
+        sgm_data = fix_hex_length(self.data, 18)
+        sgm_xor = fix_hex_length(self.sig, 1)
+        return sgm_id + sgm_data + sgm_xor
+
+    @staticmethod
+    def generate_rgb_packet(r, g, b):
+        structure = GoveePacket(51)
+        structure.set_data([5, 2, r, g, b, 0, 255, 174, 84])
+        return structure.generate_packet()
+
+def fix_hex_length(intv, length):
+    h = hex(intv).replace("0x", "")
+    h = (length * 2 - len(h)) * "0" + h
+    return h
+
+packet = GoveePacket.generate_rgb_packet(r, g, b)
+print(packet)
