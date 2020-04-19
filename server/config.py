8da1c3b998919d4d6f609626b01932eecbfbbf40
fboyle2001 <7895247+fboyle2001@users.noreply.github.com>
2020-04-19 16:26:43 +0100

    Fixed linux mode detection

--- server/config.py
+++ server/config.py
@@ -5,7 +5,9 @@ config = {
 
 import os.path
 
-if os.path.isfile("linux_mode.config"):
+linux_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "linux_mode.config")
+
+if os.path.isfile(linux_path):
     print("Using Linux mode")
     config["linux"] = True
 else:
