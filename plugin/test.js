3d1859610836a3fcfd11f0db19047a8b2e242ff9
fboyle2001 <7895247+fboyle2001@users.noreply.github.com>
2020-04-26 14:00:51 +0100

    Fix status codes

--- plugin/test.js
+++ plugin/test.js
@@ -6,19 +6,23 @@ function attempt(url) {
   return ;
 }
 
-got("http://10.0.0.20:5000/off?device=A4:C1:38:A0:7B:19").then(a => {
+got("http://10.0.0.20:5000/on?device=A4:C1:38:A0:7B:19").then(a => {
   a = JSON.parse(a.body);
   console.log(a.status.code);
   if(a.status.code == 200) {
     return;
   }
   console.log("Single failure");
-  got("http://10.0.0.20:5000/off?device=A4:C1:38:A0:7B:19").then(b => {
+  got("http://10.0.0.20:5000/on?device=A4:C1:38:A0:7B:19").then(b => {
     b = JSON.parse(b.body);
     console.log(b.status.code);
     if(b.status.code == 200) {
       return;
     }
     console.log("Double failure");
+  }).catch(error => {
+    console.log(error);
   });
+}).catch(error => {
+  console.log(error);
 });
