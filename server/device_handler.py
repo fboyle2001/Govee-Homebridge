f673c9c3106a1167c5511b0c25fcde9521f72011
fboyle2001 <7895247+fboyle2001@users.noreply.github.com>
2020-04-27 11:29:25 +0100

    Logging

--- server/device_handler.py
+++ server/device_handler.py
@@ -4,6 +4,23 @@ import threading
 import packets
 import pexpect
 import time
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+console_format = logging.Formatter('%(asctime)s %(name)s [%(levelname)s] - %(message)s')
+console_handler.setFormatter(console_format)
+
+file_handler = logging.FileHandler("app.log")
+file_handler.setLevel(logging.DEBUG)
+file_format = logging.Formatter('%(asctime)s %(name)s:%(funcName)20s():%(lineno)s [%(levelname)s] - %(message)s')
+file_handler.setFormatter(file_format)
+
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
 
 class PExpectMock:
     def sendline(self, data):
@@ -34,20 +51,27 @@ class DevicePacketProcessor:
 
     def queue_packet(self, packet, callback, value):
         self.waiting_packets.put((packet, callback, value))
+        logger.debug(f"Queued {packet} on {self.device.mac}, using callback {callback} with arg {value}")
 
     def stop_processing(self):
         self.active = False
+        logger.info(f"Stopped packet processing for {self.device.mac}")
 
     def start_processing(self):
         self.processing_thread = threading.Thread(target=self.process_packets)
         self.processing_thread.setDaemon(True)
         self.processing_thread.start()
+        logger.info(f"Started packet processing for {self.device.mac}")
 
     def process_packets(self):
         self.active = True
         gatt_instance = PExpectMock()
         if config["linux"]:
+            logger.debug(f"Using Linux configuration so using gatttool")
             gatt_instance = pexpect.spawn("gatttool -I")
+        else:
+            logger.debug(f"Using Windows configuration so using PExpectMock")
+
         connected = False
 
         for i in range(self.max_connect_attempts):
@@ -58,33 +82,43 @@ class DevicePacketProcessor:
                 connected = True
                 break
             except pexpect.exceptions.TIMEOUT:
-                pass
+                attempt = i + 1
+                logger.warning(f"Failed to connect to {self.device.mac}, attempt {attempt}/{self.max_connect_attempts}")
 
         if not connected:
+            logger.critical(f"Unable to connect to {self.device.mac}")
             raise ConnectionError("Unable to connect to " + self.device.mac)
 
         while self.active:
             while self.waiting_packets.qsize() != 0:
                 packet, callback, value = self.waiting_packets.get()
+                logger.debug(f"Found {packet} to send to {self.device.mac}")
                 gatt_instance.sendline(f"char-write-cmd 0x0015 {packet}")
                 gatt_instance.expect(".*")
                 self.waiting_packets.task_done()
+                logger.debug(f"Sent {packet} to {self.device.mac}")
                 if callback != None:
                     callback(self.device, value)
                 time.sleep(self.delay_packet_period)
 
+            logger.debug(f"Depleted waiting packet queue for {self.device.mac}")
+
             gatt_instance.sendline(packets.GoveePacket.keep_alive_packet())
             gatt_instance.expect(".*")
+
+            logger.debug(f"Sent keep alive packet to {self.device.mac}")
 
             time.sleep(self.send_alive_packet_period)
 
         gatt_instance.sendline("disconnect")
         gatt_instance.expect(".*")
+        logger.info(f"Disconnected from {self.device.mac}")
 
 DEVICE_REGISTER = {}
 
 def get_device(mac):
     if mac not in DEVICE_REGISTER:
         DEVICE_REGISTER[mac] = Device(mac)
+        logger.info(f"Registered new device with mac address {mac}")
 
     return DEVICE_REGISTER[mac]
--- server/request_receiver.py
+++ server/request_receiver.py
@@ -3,8 +3,25 @@ from request_receiver_util import *
 from config import config
 import device_handler
 import packets
+import logging
 
 app = Flask(__name__)
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+console_format = logging.Formatter('%(asctime)s [%(levelname)s] - %(message)s')
+console_handler.setFormatter(console_format)
+
+file_handler = logging.FileHandler("app.log")
+file_handler.setLevel(logging.DEBUG)
+file_format = logging.Formatter('%(asctime)s %(name)s:%(funcName)20s():%(lineno)s [%(levelname)s] - %(message)s')
+file_handler.setFormatter(file_format)
+
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
 
 @app.route("/api/update/on")
 def turn_light_on():
@@ -12,18 +29,21 @@ def turn_light_on():
     mac = request.args.get("mac")
 
     if mac == None:
-        response.set_status(400, "Missing mac address")
+        logger.warning("Rejected request to turn light on: missing MAC address")
+        response.set_status(400, "Missing MAC address")
         return response.display()
 
     packet = packets.GoveePacket.on_packet()
     device = device_handler.get_device(mac)
     device.packet_processor.queue_packet(packet, update_light_on_callback, None)
     response.set_status(200)
+    logger.info(f"Queued on packet for {mac} and sending 200 response")
 
     return response.display()
 
 def update_light_on_callback(device, value):
     device.on = True
+    logger.info(f"On packet sent to {device.mac}. Callback called, updated on value to True")
 
 @app.route("/api/update/off")
 def turn_light_off():
@@ -31,18 +51,21 @@ def turn_light_off():
     mac = request.args.get("mac")
 
     if mac == None:
-        response.set_status(400, "Missing mac address")
+        logger.warning("Rejected request to turn light off: missing MAC address")
+        response.set_status(400, "Missing MAC address")
         return response.display()
 
     packet = packets.GoveePacket.off_packet()
     device = device_handler.get_device(mac)
     device.packet_processor.queue_packet(packet, update_light_off_callback, None)
     response.set_status(200)
+    logger.info(f"Queued off packet for {mac} and sending 200 response")
 
     return response.display()
 
 def update_light_off_callback(device, value):
     device.on = False
+    logger.info(f"Off packet sent to {device.mac}. Callback called, updated on value to False")
 
 @app.route("/api/update/brightness")
 def update_brightness():
@@ -51,10 +74,12 @@ def update_brightness():
     brightness = request.args.get("brightness")
 
     if mac == None:
-        response.set_status(400, "Missing mac address")
+        logger.warning("Rejected request to update brightness: missing MAC address")
+        response.set_status(400, "Missing MAC address")
         return response.display()
 
     if brightness == None:
+        logger.warning("Rejected request to update brightness: missing brightness level")
         response.set_status(400, "Missing brightness")
         return response.display()
     else:
@@ -66,11 +91,13 @@ def update_brightness():
     device = device_handler.get_device(mac)
     device.packet_processor.queue_packet(packet, update_brightness_callback, brightness)
     response.set_status(200)
+    logger.info(f"Queued brightness packet for {mac} with brightness level {brightness} and sending 200 response")
 
     return response.display()
 
-def update_brightness_callback(device, value):
-    device.brightness = value
+def update_brightness_callback(device, brightness):
+    device.brightness = brightness
+    logger.info(f"Brightness packet sent to {device.mac}. Callback called, updated brightness value to {brightness}")
 
 @app.route("/api/read/brightness")
 def get_brightness():
@@ -78,11 +105,13 @@ def get_brightness():
     mac = request.args.get("mac")
 
     if mac == None:
-        response.set_status(400, "Missing mac address")
+        logger.warning("Rejected request to read brightness: missing MAC address")
+        response.set_status(400, "Missing MAC address")
         return response.display()
 
     device = device_handler.get_device(mac)
     response.set_status(200)
     response.set_data("brightness", device.brightness)
+    logger.info(f"Read brightness for {mac}, sending 200 response")
 
     return response.display()
